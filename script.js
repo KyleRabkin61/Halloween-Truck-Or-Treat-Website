6416331fc0bd3b1572d20ad3589a2915545555c7
KyleRabkin61 <kylerabkin6161@gmail.com>
2024-10-15 13:48:46 -0400

    shortened j query, fixed blocking elements within divs, started created more info btn.

--- script.js
+++ script.js
@@ -1,57 +1,9 @@
 $('document').ready(() => {
-    $('.pic1').on('mouseenter', () => {
-        $('.pic-descrip1').fadeIn()
-    }).on('mouseleave', () => {
-        $('.pic-descrip1').fadeOut()
-    })
-
-    $('.pic2').on('mouseenter', () => {
-        $('.pic-descrip2').fadeIn()
-    }).on('mouseleave', () => {
-        $('.pic-descrip2').fadeOut()
-    })
-
-    $('.pic3').on('mouseenter', () => {
-        $('.pic-descrip3').fadeIn()
-    }).on('mouseleave', () => {
-        $('.pic-descrip3').fadeOut()
-    })
-
-    $('.pic4').on('mouseenter', () => {
-        $('.pic-descrip4').fadeIn()
-    }).on('mouseleave', () => {
-        $('.pic-descrip4').fadeOut()
-    })
-
-    $('.pic5').on('mouseenter', () => {
-        $('.pic-descrip5').fadeIn()
-    }).on('mouseleave', () => {
-        $('.pic-descrip5').fadeOut()
-    })
-
-    $('.pic6').on('mouseenter', () => {
-        $('.pic-descrip6').fadeIn()
-    }).on('mouseleave', () => {
-        $('.pic-descrip6').fadeOut()
-    })
-
-    $('.pic7').on('mouseenter', () => {
-        $('.pic-descrip7').fadeIn()
-    }).on('mouseleave', () => {
-        $('.pic-descrip7').fadeOut()
-    })
-
-    $('.pic8').on('mouseenter', () => {
-        $('.pic-descrip8').fadeIn()
-    }).on('mouseleave', () => {
-        $('.pic-descrip8').fadeOut()
-    })
-
-    $('.pic9').on('mouseenter', () => {
-        $('.pic-descrip9').fadeIn()
-    }).on('mouseleave', () => {
-        $('.pic-descrip9').fadeOut()
-    })
+    $('.pic').on('mouseenter', function() {
+        $(this).find('.pic-descrip').fadeIn();
+    }).on('mouseleave', function() {
+        $(this).find('.pic-descrip').fadeOut();
+    });
 })
 
 // audio for join btn

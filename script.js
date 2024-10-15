9db30e267f572e0a559ca5f362b9a0b9ddfe1e73
KyleRabkin61 <kylerabkin6161@gmail.com>
2024-10-14 21:44:22 -0400

    created j query fadein/out effect on text

--- script.js
+++ script.js
@@ -1,5 +1,57 @@
 $('document').ready(() => {
+    $('.pic1').on('mouseenter', () => {
+        $('.pic-descrip').fadeIn()
+    }).on('mouseleave', () => {
+        $('.pic-descrip').fadeOut()
+    })
 
+    $('.pic2').on('mouseenter', () => {
+        $('.pic-descrip').fadeIn()
+    }).on('mouseleave', () => {
+        $('.pic-descrip').fadeOut()
+    })
+
+    $('.pic3').on('mouseenter', () => {
+        $('.pic-descrip').fadeIn()
+    }).on('mouseleave', () => {
+        $('.pic-descrip').fadeOut()
+    })
+
+    $('.pic4').on('mouseenter', () => {
+        $('.pic-descrip').fadeIn()
+    }).on('mouseleave', () => {
+        $('.pic-descrip').fadeOut()
+    })
+
+    $('.pic5').on('mouseenter', () => {
+        $('.pic-descrip').fadeIn()
+    }).on('mouseleave', () => {
+        $('.pic-descrip').fadeOut()
+    })
+
+    $('.pic6').on('mouseenter', () => {
+        $('.pic-descrip').fadeIn()
+    }).on('mouseleave', () => {
+        $('.pic-descrip').fadeOut()
+    })
+
+    $('.pic7').on('mouseenter', () => {
+        $('.pic-descrip').fadeIn()
+    }).on('mouseleave', () => {
+        $('.pic-descrip').fadeOut()
+    })
+
+    $('.pic8').on('mouseenter', () => {
+        $('.pic-descrip').fadeIn()
+    }).on('mouseleave', () => {
+        $('.pic-descrip').fadeOut()
+    })
+
+    $('.pic9').on('mouseenter', () => {
+        $('.pic-descrip').fadeIn()
+    }).on('mouseleave', () => {
+        $('.pic-descrip').fadeOut()
+    })
 })
 
 // audio for join btn

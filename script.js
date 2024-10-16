5a6decf7da5474c06a433d4afa75de2173caf064
KyleRabkin61 <kylerabkin6161@gmail.com>
2024-10-16 18:20:35 -0400

    Added back to top button as well as fixed up showcase page

--- script.js
+++ script.js
@@ -6,11 +6,32 @@ $('document').ready(() => {
     });
 
     $('.more-info-btn').on('click', function() {
-        $('.flyer').fadeToggle()
+        $('.flyer').fadeIn()
     })
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () { scrollFunction() };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 56 || document.documentElement.scrollTop > 56) {
+            $('#myBtn').fadeIn(200)
+        } else {
+            $('#myBtn').fadeOut(200)
+        }
+}
+
+
 })
 
 // audio for join btn
 
 const audio = new Audio();
 audio.src = "audio/evil-laugh.mp3";
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
